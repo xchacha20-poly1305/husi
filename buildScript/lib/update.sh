55e58ecca995356a3f73a115c674fb2dd425c351
HystericalDragon <HystericalDragons@proton.me>
2023-12-31 20:36:50 +0800

    build: reset submodle when update

--- buildScript/lib/update.sh
+++ buildScript/lib/update.sh
@@ -2,4 +2,25 @@
 
 set -e
 
+RESET_GIT() {
+	local BRANCH=$1
+	git checkout $BRANCH
+	git reset --hard origin/$BRANCH
+}
+
 git submodule update --remote
+
+pushd dun/
+
+RESET_GIT dev
+
+pushd sing-box/
+RESET_GIT def
+popd
+
+pushd sing-quic
+RESET_GIT dev
+popd
+
+popd
+
