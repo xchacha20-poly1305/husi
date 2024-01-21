8db421363316f06712489af1c72332bff15f12a5
HystericalDragon <HystericalDragons@proton.me>
2024-01-21 15:59:18 +0800

    build(libcore): show libcore sha256 About #1
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/init.sh
+++ libcore/init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -e
+
+[[ -e "inited" ]] && exit 0
 
 # Install gomobile
 if [ ! -f "$GOPATH/bin/gomobile" ]; then
-    go install golang.org/x/mobile/cmd/gomobile@latest
-    go install golang.org/x/mobile/cmd/gobind@latest
+	go install golang.org/x/mobile/cmd/gomobile@latest
+	go install golang.org/x/mobile/cmd/gobind@latest
 fi
 
 gomobile init
+touch inited
