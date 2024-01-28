4c511f3816f6f6e59f8db2aa9c62403207777838
HystericalDragon <HystericalDragons@proton.me>
2024-01-28 21:26:01 +0800

    feat(hysteria2): allow share sha256 cert in share link
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/crypto.go
+++ libcore/crypto.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"strings"
 )
 
 func Sha1(data []byte) []byte {
@@ -14,4 +15,21 @@ func Sha1(data []byte) []byte {
 func Sha256Hex(data []byte) string {
 	sum := sha256.Sum256(data)
 	return hex.EncodeToString(sum[:])
+}
+
+// BA:88:45:17:A1
+func Sha256OpenSSL(data []byte) string {
+	sum := sha256.Sum256(data)
+	sumStr := hex.EncodeToString(sum[:])
+	sumStr = strings.ToUpper(sumStr)
+
+	sumSlice := strings.Split(sumStr, "")
+
+	var result []string
+	for i := 0; i < len(sumSlice)-1; i += 2 {
+		appendSlice := strings.Join(sumSlice[i:i+2], "")
+		result = append(result, appendSlice)
+	}
+
+	return strings.Join(result, ":")
 }
