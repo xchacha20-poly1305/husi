5d45be4b0f69d6c47d57038e49333c6eaf822fb8
HystericalDragon <HystericalDragons@proton.me>
2024-02-03 17:28:35 +0800

    refactor(libcore): use Once to "lazy load" C.Version fix #29
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/version.go
+++ libcore/version.go
@@ -4,42 +4,64 @@ import (
 	"runtime"
 	"runtime/debug"
 	"strings"
+	"sync"
 
 	C "github.com/sagernet/sing-box/constant"
 )
 
-const VERSION = "v1.8.4"
+const SingBoxPath = "github.com/sagernet/sing-box"
 
+// There are some other place will read C.Version, such as Clash API.
+// So load it in init()
 func init() {
-	C.Version = VERSION
+	buildInfo, _ := debug.ReadBuildInfo()
+	for _, dep := range buildInfo.Deps {
+		switch dep.Path {
+		case SingBoxPath:
+			C.Version = dep.Version
+			return
+		}
+	}
 }
+
+// VersionBox returns sing-box version
+func VersionBox() string {
+	return C.Version
+}
+
+var (
+	detailVersion     string
+	detailVersionOnce sync.Once
+)
 
 // Version
 // Show detail version
-//
 // Format:
 //
 //	sing-box: {dun_version}
 //	{go_version}@{os}/{arch}
 //	{tags}
 func Version() string {
-	detailVersion := []string{
-		"sing-box: " + VERSION,
+	detailVersionOnce.Do(loadDetailVersion)
+	return detailVersion
+}
+
+func loadDetailVersion() {
+	detailVersionSli := []string{
+		"sing-box: " + C.Version,
 		runtime.Version() + "@" + runtime.GOOS + "/" + runtime.GOARCH,
 	}
 
-	debugInfo, loaded := debug.ReadBuildInfo()
-	if loaded {
-		for _, setting := range debugInfo.Settings {
-			switch setting.Key {
-			case "-tags":
-				if setting.Value != "" {
-					detailVersion = append(detailVersion, setting.Value)
-					break
-				}
+	debugInfo, _ := debug.ReadBuildInfo()
+	for _, setting := range debugInfo.Settings {
+		switch setting.Key {
+		case "-tags":
+			if setting.Value != "" {
+				detailVersionSli = append(detailVersionSli, setting.Value)
+				break
 			}
 		}
 	}
 
-	return strings.Join(detailVersion, "\n")
+	detailVersion = strings.Join(detailVersionSli, "\n")
 }
