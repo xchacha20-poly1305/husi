468d85c53aa940064cbf16f906532114f520823a
HystericalDragon <HystericalDragons@proton.me>
2024-02-25 12:08:57 +0800

    build: generate assets
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- buildScript/lib/assets.sh
+++ buildScript/lib/assets.sh
@@ -2,42 +2,37 @@
 
 set -e
 
-DIR=app/src/main/assets/sing-box
+source buildScript/init/version.sh
+
+DIR="app/src/main/assets/sing-box"
+GENERATER="libcore/cmd/ruleset_generate"
 rm -rf $DIR
 mkdir -p $DIR
+
+# get_latest_release() {
+#   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+#     grep '"tag_name":' |                                            # Get tag line
+#     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+# }
+
+function pack() {
+  local name=$1
+  zip -r -q -X "${name}.zip" "ruleset/$name"
+}
+
+echo "GEOIP: $GEOIP_VERSION"
+echo "GEOSITE: $GEOSITE_VERSION"
+pushd $GENERATER
+rm -rf ruleset
+rm *.zip
+go run . -geoip=$GEOIP_VERSION -geosite=$GEOSITE_VERSION || exit 1
+pack "geoip"
+pack "geosite"
+popd
+
+cp -r "$GENERATER/geoip.zip" "$DIR"
+cp -r "$GENERATER/geosite.zip" "$DIR"
+
 cd $DIR
-
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
-}
-
-get_rule_set() {
-  local type=$1 # example: geoip
-  local zip_name="${type}.zip"
-  local rule_set_name="sing-${type}-rule-set"
-
-  unzip -q $zip_name
-  rm $zip_name
-  mv $rule_set_name $type
-  zip -r -q -X $zip_name $type -9
-  rm -rf $type
-}
-
-####
-# VERSION_GEOIP=`get_latest_release "SagerNet/sing-geoip"`
-VERSION_GEOIP=$(date +%Y%m%d)
-echo VERSION_GEOIP=$VERSION_GEOIP
-echo -n $VERSION_GEOIP >geoip.version.txt
-curl -fLSso geoip.zip https://codeload.github.com/SagerNet/sing-geoip/zip/refs/heads/rule-set
-get_rule_set geoip
-# xz -9 geoip.db
-
-####
-VERSION_GEOSITE=$(date "+%Y%m%d")
-echo VERSION_GEOSITE=$VERSION_GEOSITE
-echo -n $VERSION_GEOSITE >geosite.version.txt
-curl -fLSso geosite.zip https://codeload.github.com/SagerNet/sing-geosite/zip/refs/heads/rule-set
-get_rule_set geosite
-# xz -9 geosite.db
+echo -n $GEOIP_VERSION >geoip.version.txt
+echo -n $GEOSITE_VERSION >geosite.version.txt
