86b8428796744d5d0e1fcbbe705ce59d539f8b2e
HystericalDragon <HystericalDragons@proton.me>
2024-02-25 09:51:42 +0800

    Disable CGO for gomobile
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/init.sh
+++ libcore/init.sh
@@ -5,8 +5,8 @@ set -e
 
 # Install gomobile
 if [ ! -f "$GOPATH/bin/gomobile" ]; then
-	go install -v -trimpath -ldflags="-w -s" github.com/sagernet/gomobile/cmd/gobind@v0.1.3
-	go install -v -trimpath -ldflags="-w -s" github.com/sagernet/gomobile/cmd/gomobile@v0.1.3
+	CGO_ENABLED=0 go install -v -trimpath -ldflags="-w -s" github.com/sagernet/gomobile/cmd/gobind@v0.1.3
+	CGO_ENABLED=0 go install -v -trimpath -ldflags="-w -s" github.com/sagernet/gomobile/cmd/gomobile@v0.1.3
 fi
 
 gomobile init
