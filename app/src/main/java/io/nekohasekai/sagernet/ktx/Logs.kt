d574b8d42903686836d11ae524e378317e584a19
HystericalDragon <HystericalDragons@proton.me>
2024-03-16 18:05:23 +0800

    refactor(log): print log with level by sing-box/log
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Logs.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Logs.kt
@@ -11,46 +11,44 @@ object Logs {
         return stackTrace[4].className.substringAfterLast(".")
     }
 
-    // level int use logrus.go
-
     fun d(message: String) {
-        Libcore.logPrintln("[Debug] [${mkTag()}] $message")
+        Libcore.logDebug("[${mkTag()}] $message")
     }
 
     fun d(message: String, exception: Throwable) {
-        Libcore.logPrintln("[Debug] [${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        Libcore.logDebug("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
     }
 
     fun i(message: String) {
-        Libcore.logPrintln("[Info] [${mkTag()}] $message")
+        Libcore.logInfo("[${mkTag()}] $message")
     }
 
     fun i(message: String, exception: Throwable) {
-        Libcore.logPrintln("[Info] [${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        Libcore.logInfo("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
     }
 
     fun w(message: String) {
-        Libcore.logPrintln("[Warning] [${mkTag()}] $message")
+        Libcore.logWarning("[${mkTag()}] $message")
     }
 
     fun w(message: String, exception: Throwable) {
-        Libcore.logPrintln("[Warning] [${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        Libcore.logWarning("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
     }
 
     fun w(exception: Throwable) {
-        Libcore.logPrintln("[Warning] [${mkTag()}] " + exception.stackTraceToString())
+        Libcore.logWarning("[${mkTag()}] " + exception.stackTraceToString())
     }
 
     fun e(message: String) {
-        Libcore.logPrintln("[Error] [${mkTag()}] $message")
+        Libcore.logError("[${mkTag()}] $message")
     }
 
     fun e(message: String, exception: Throwable) {
-        Libcore.logPrintln("[Error] [${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        Libcore.logError("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
     }
 
     fun e(exception: Throwable) {
-        Libcore.logPrintln("[Error] [${mkTag()}] " + exception.stackTraceToString())
+        Libcore.logError("[${mkTag()}] " + exception.stackTraceToString())
     }
 
 }
