919e6f8fdf095e673c2c8193369a2a689121e8e8
HystericalDragon <HystericalDragons@proton.me>
2024-03-23 20:43:46 +0800

    chore: remove unused
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- libcore/hash.go
+++ libcore/hash.go
@@ -1,16 +1,10 @@
 package libcore
 
 import (
-	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
 	"strings"
 )
-
-func Sha1(data []byte) []byte {
-	sum := sha1.Sum(data)
-	return sum[:]
-}
 
 func Sha256Hex(data []byte) string {
 	sum := sha256.Sum256(data)
