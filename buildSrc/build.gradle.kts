5d8cc021a935d417bcbd8034d8fe03b131bf2c63
HystericalDragon <HystericalDragons@proton.me>
2024-04-13 18:26:41 +0800

    revert: "chore: upgrade AGP 8.3"
    Waitting for IDEA supported it.
    
    This reverts commit 06942ea80a7e5548908d2e2dfe7c73570cb47f6b.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     // Gradle Plugins
-    implementation("com.android.tools.build:gradle:8.3.0")
+    implementation("com.android.tools.build:gradle:8.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
 }
