4f21e10bb07e49b460192f8b31765655be4a4744
HystericalDragon <HystericalDragons@proton.me>
2024-05-11 20:04:58 +0800

    chore: update dependencies
    * Update go to v1.22.3
    * Update geosite
    * Update sing-box to v1.9.0-rc.16
    
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.22.2"
+GO_VERSION="1.22.3"
 
 # https://github.com/Dreamacro/maxmind-geoip
 GEOIP_VERSION="20240412"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20240426060244"
+GEOSITE_VERSION="20240508170917"
