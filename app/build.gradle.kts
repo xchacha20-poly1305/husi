6a838983de66509c9ee2363dc07cb93ff47240a6
HystericalDragon <HystericalDragons@proton.me>
2024-06-15 14:52:51 +0800

    chore: update AGP to 8.5
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.0")
     implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -74,9 +74,9 @@ dependencies {
     implementation("com.blacksquircle.ui:language-base:2.9.0")
     implementation("com.blacksquircle.ui:language-json:2.9.0")
 
-    implementation("androidx.camera:camera-view:1.3.3")
-    implementation("androidx.camera:camera-lifecycle:1.3.3")
-    implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation("androidx.camera:camera-view:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
     implementation("com.google.zxing:core:3.5.3")
 
     implementation("com.github.daniel-stoneuk:material-about-library:3.2.0-rc01")
