d569c47974697ded3363397bf70add160a623e84
HystericalDragon <HystericalDragons@proton.me>
2024-07-06 21:59:17 +0800

    REVERT: DOWNGRADE GO VERSIOn to 1.22.4
    fatal error: runtime: stack split at bad time
    
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- buildScript/init/action/go.sh
+++ buildScript/init/action/go.sh
@@ -5,8 +5,8 @@ source buildScript/init/env.sh
 mkdir -p $PWD/build/golang
 cd $golang
 
-curl -Lso go.tar.gz https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-echo "904b924d435eaea086515bc63235b192ea441bd8c9b198c507e85009e6e4c7f0 go.tar.gz" | sha256sum -c -
+curl -Lso go.tar.gz https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+echo "ba79d4526102575196273416239cca418a651e049c2b099f3159db85e7bade7d go.tar.gz" | sha256sum -c -
 tar xzf go.tar.gz
 
 go version
