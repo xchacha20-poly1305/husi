2fcb74cc56ec8f7e17fa8814d8ac5dff90e38f25
HystericalDragon <HystericalDragons@proton.me>
2024-07-06 19:43:50 +0800

    chore: update dependencies
    * Update go to 1.22.5
    * Update geosite
    
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.22.4"
+GO_VERSION="1.22.5"
 
 # https://github.com/Dreamacro/maxmind-geoip
 GEOIP_VERSION="20240612"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20240621160143"
+GEOSITE_VERSION="20240624143214"
