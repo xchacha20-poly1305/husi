6a5091e9a6e7a48c9ed27cdd2c1e898a8847be91
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-01-16 15:10:40 +0800

    chore: update AGP 8.8.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,3 +18,18 @@ when {
 include(":app")
 
 rootProject.name = "husi"
+
+// https://issuetracker.google.com/issues/389508413
+pluginManagement {
+    buildscript {
+        repositories {
+            mavenCentral()
+            maven {
+                url = uri("https://storage.googleapis.com/r8-releases/raw")
+            }
+        }
+        dependencies {
+            classpath("com.android.tools:r8:8.8.27")
+        }
+    }
+}
