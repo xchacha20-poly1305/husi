90b1c4ac5608c5745383ce9dd7bedfec2daf1e5c
安容 <HystericalDragons@proton.me>
2025-02-11 14:50:36 +0800

    chore(preference): add some default values &
    rename TempDatabase

--- app/src/main/java/io/nekohasekai/sagernet/database/TempDatabase.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/TempDatabase.kt
@@ -1,19 +1,18 @@
-package moe.matsuri.nb4a
+package io.nekohasekai.sagernet.database
 
-import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import io.nekohasekai.sagernet.SagerNet
 import io.nekohasekai.sagernet.database.preference.KeyValuePair
+import io.nekohasekai.sagernet.ktx.app
 import io.nekohasekai.sagernet.ktx.runOnDefaultDispatcher
 
-@Database(entities = [KeyValuePair::class], version = 1)
+@androidx.room.Database(entities = [KeyValuePair::class], version = 1)
 abstract class TempDatabase : RoomDatabase() {
 
     companion object {
         @Suppress("EXPERIMENTAL_API_USAGE")
         private val instance by lazy {
-            Room.inMemoryDatabaseBuilder(SagerNet.application, TempDatabase::class.java)
+            Room.inMemoryDatabaseBuilder(app, TempDatabase::class.java)
                 .allowMainThreadQueries()
                 .fallbackToDestructiveMigration()
                 .setQueryExecutor { runOnDefaultDispatcher { it.run() } }
