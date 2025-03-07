93f71e129c2ff69c11cde8047d703b6fb4ac1aaf
安容 <HystericalDragons@proton.me>
2025-03-07 20:34:37 +0800

    chore: update dependencies
    * Update geosite
    * Update AGP and gradle
    * Update Go

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.24.0"
+GO_VERSION="1.24.1"
 
 # https://github.com/Dreamacro/maxmind-geoip
 GEOIP_VERSION="20250212"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20250227085631"
+GEOSITE_VERSION="20250307074130"
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     // Gradle Plugins
-    implementation("com.android.tools.build:gradle:8.8.2")
+    implementation("com.android.tools.build:gradle:8.9.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
 }
