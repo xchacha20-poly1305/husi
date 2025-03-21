8878c86cbb425cd53efd0b6f1bbf026ce4eb2e49
安容 <HystericalDragons@proton.me>
2025-03-21 16:32:03 +0800

    build: update gomobile init usage

--- buildScript/lib/core/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "buildScript/init/env.sh"
-export CGO_ENABLED=1
-export GO386=softfloat
-
-cd libcore
-rel=1 ./build.sh || exit 1
--- buildScript/lib/core/init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "buildScript/init/env.sh"
-
-[ -f libcore/go.mod ] || exit 1
-cd libcore
-
-./init.sh || exit 1
