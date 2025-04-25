dc0e5b266067475fd8a6b00317e288efdc9b0940
安容 <HystericalDragons@proton.me>
2025-04-25 13:09:48 +0800

    feat: add orientation locking during tests and interrupt tests if configuration changes

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Dialogs.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Dialogs.kt
@@ -1,7 +1,10 @@
 package io.nekohasekai.sagernet.ktx
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -16,3 +19,19 @@ fun Context.alert(text: String): AlertDialog {
 
 fun Fragment.alert(text: String) = requireContext().alert(text)
 
+@SuppressLint("SourceLockedOrientationActivity")
+fun Activity.blockOrientation() {
+    when (resources.configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        else -> {}
+    }
+}
+
+fun Activity.unlockOrientation() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+}
