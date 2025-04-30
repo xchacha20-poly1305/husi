5680b7b9ca41f526c2b161eef428cbb4962a2eb6
安容 <HystericalDragons@proton.me>
2025-04-30 21:25:02 +0800

    fix: also update PublicDatabase fallback usage

--- app/src/main/java/io/nekohasekai/sagernet/database/preference/PublicDatabase.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/preference/PublicDatabase.kt
@@ -5,19 +5,20 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import dev.matrix.roomigrant.GenerateRoomMigrations
 import io.nekohasekai.sagernet.Key
-import io.nekohasekai.sagernet.SagerNet
 import io.nekohasekai.sagernet.ktx.runOnDefaultDispatcher
+import io.nekohasekai.sagernet.ktx.app
 
 @Database(entities = [KeyValuePair::class], version = 1)
 @GenerateRoomMigrations
 abstract class PublicDatabase : RoomDatabase() {
     companion object {
         val instance by lazy {
-            SagerNet.application.getDatabasePath(Key.DB_PROFILE).parentFile?.mkdirs()
-            Room.databaseBuilder(SagerNet.application, PublicDatabase::class.java, Key.DB_PUBLIC)
+            app.getDatabasePath(Key.DB_PROFILE).parentFile?.mkdirs()
+            Room.databaseBuilder(app, PublicDatabase::class.java, Key.DB_PUBLIC)
                 .allowMainThreadQueries()
                 .enableMultiInstanceInvalidation()
-                .fallbackToDestructiveMigration()
+                .fallbackToDestructiveMigration(true)
+		.fallbackToDestructiveMigrationOnDowngrade(true)
                 .setQueryExecutor { runOnDefaultDispatcher { it.run() } }
                 .build()
         }
