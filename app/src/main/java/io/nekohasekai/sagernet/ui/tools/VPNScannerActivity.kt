c6c288ccc8a4fb092cba8d843161d9e9a0d2834c
安容 <HystericalDragons@proton.me>
2025-06-21 21:00:12 +0800

    fix: replace bad log usage

--- app/src/main/java/io/nekohasekai/sagernet/ui/tools/VPNScannerActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/tools/VPNScannerActivity.kt
@@ -8,7 +8,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
@@ -21,6 +20,7 @@ import com.android.tools.smali.dexlib2.dexbacked.DexBackedDexFile
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.databinding.LayoutVpnScannerBinding
 import io.nekohasekai.sagernet.databinding.ViewVpnAppItemBinding
+import io.nekohasekai.sagernet.ktx.Logs
 import io.nekohasekai.sagernet.ktx.onMainDispatcher
 import io.nekohasekai.sagernet.ktx.toStringIterator
 import io.nekohasekai.sagernet.ui.ThemedActivity
@@ -251,7 +251,7 @@ class VPNScannerActivity : ThemedActivity() {
                 val dexFile = try {
                     DexBackedDexFile.fromInputStream(null, input)
                 } catch (e: Exception) {
-                    Log.e("VPNScanActivity", "Failed to read dex file", e)
+                    Logs.e("Read dex file", e)
                     continue
                 }
                 for (clazz in dexFile.classes) {
