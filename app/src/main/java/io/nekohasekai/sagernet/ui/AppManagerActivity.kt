e1fe909691310c32a92758f17588fb184e2fcb87
安容 <HystericalDragons@proton.me>
2025-06-22 11:11:20 +0800

    fix: select China apps in proxy mode
    (cherry picked from commit 8bc546013371305fba1d9dfc8e13068d949b83c4)

--- app/src/main/java/io/nekohasekai/sagernet/ui/AppManagerActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/AppManagerActivity.kt
@@ -529,6 +529,12 @@ class AppManagerActivity : ThemedActivity() {
                     } else {
                         proxiedUids.delete(app.applicationInfo!!.uid)
                     }
+                } else {
+                    if (!bypass) {
+                        proxiedUids[app.applicationInfo!!.uid] = true
+                    } else {
+                        proxiedUids.delete(app.applicationInfo!!.uid)
+                    }
                 }
 
             }
