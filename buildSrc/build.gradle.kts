509940102032b7278f0c1a37226a8dfba80b00f3
安容 <HystericalDragons@proton.me>
2025-06-25 11:37:35 +0800

    chore: update AGP 8.11.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     // Gradle Plugins
-    implementation("com.android.tools.build:gradle:8.10.1")
+    implementation("com.android.tools.build:gradle:8.11.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
 }
