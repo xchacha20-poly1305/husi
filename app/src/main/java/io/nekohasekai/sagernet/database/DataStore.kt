8a7ce69767008176c05a2d833a43ffb2af1da55c
安容 <HystericalDragons@proton.me>
2025-06-29 18:10:47 +0800

    feat(settings): add option to ignore device idle mode

--- app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
@@ -181,6 +181,7 @@ object DataStore : OnPreferenceDataStoreChangeListener {
     var tunImplementation by configurationStore.stringToInt(Key.TUN_IMPLEMENTATION) { TunImplementation.MIXED }
     var profileTrafficStatistics by configurationStore.boolean(Key.PROFILE_TRAFFIC_STATISTICS) { true }
     var certProvider by configurationStore.stringToInt(Key.CERT_PROVIDER) { CertProvider.MOZILLA }
+    var ignoreDeviceIdle by configurationStore.boolean(Key.IGNORE_DEVICE_IDLE)
 
     var trafficDescending by configurationStore.boolean(Key.TRAFFIC_DESCENDING) { false }
     var trafficSortMode by configurationStore.int(Key.TRAFFIC_SORT_MODE) { TrafficSortMode.START }
