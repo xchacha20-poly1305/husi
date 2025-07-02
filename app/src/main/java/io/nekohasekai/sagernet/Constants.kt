7942d274db87da7cb15cb99104a926459f126b64
安容 <HystericalDragons@proton.me>
2025-07-02 08:52:17 +0800

    refactor: remove all long click preference

--- app/src/main/java/io/nekohasekai/sagernet/Constants.kt
+++ app/src/main/java/io/nekohasekai/sagernet/Constants.kt
@@ -70,7 +70,7 @@ object Key {
     const val SECURITY_ADVISORY = "securityAdvisory"
     const val TCP_KEEP_ALIVE_INTERVAL = "tcpKeepAliveInterval"
     const val LOG_LEVEL = "logLevel"
-    const val LOG_BUF_SIZE = "logBufSize"
+    const val LOG_MAX_SIZE = "logMaxSize"
     const val MTU = "mtu"
     const val ALLOW_APPS_BYPASS_VPN = "allowAppsBypassVpn"
     const val ALWAYS_SHOW_ADDRESS = "alwaysShowAddress"
