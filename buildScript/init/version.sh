7a0b0d3ab6ee37a035b441785e7d481cca31e21c
安容 <HystericalDragons@proton.me>
2025-07-20 11:39:11 +0800

    chore: update dependencies
    * Update geoip
    * Update sing-box

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -3,7 +3,7 @@
 GO_VERSION="1.24.5"
 
 # https://github.com/Dreamacro/maxmind-geoip
-GEOIP_VERSION="20250612"
+GEOIP_VERSION="20250712"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
