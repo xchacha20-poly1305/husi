8444cf2a178c761112ebd442ca82232dab310f10
安容 <HystericalDragons@proton.me>
2025-07-20 15:19:29 +0800

    chore(AlertDialogFragment): receive type class to use new API

--- app/src/main/java/com/github/shadowsocks/plugin/fragment/AlertDialogFragment.kt
+++ app/src/main/java/com/github/shadowsocks/plugin/fragment/AlertDialogFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Parcelable
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.os.BundleCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
@@ -15,23 +16,22 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
  * Based on: https://android.googlesource.com/platform/
 packages/apps/ExactCalculator/+/8c43f06/src/com/android/calculator2/AlertDialogFragment.java
  */
-abstract class AlertDialogFragment<Arg : Parcelable, Ret : Parcelable?> :
+abstract class AlertDialogFragment<Arg : Parcelable, Ret : Parcelable?>(val argClass: Class<Arg>) :
     AppCompatDialogFragment(), DialogInterface.OnClickListener {
     companion object {
         private const val KEY_RESULT = "result"
         private const val KEY_ARG = "arg"
-        private const val KEY_RET = "ret"
-        private const val KEY_WHICH = "which"
+        const val KEY_RET = "ret"
+        const val KEY_WHICH = "which"
 
-        fun <Ret : Parcelable> setResultListener(
+        inline fun <reified Ret : Parcelable> setResultListener(
             fragment: Fragment, requestKey: String,
-            listener: (Int, Ret?) -> Unit
+            crossinline listener: (Int, Ret?) -> Unit
         ) {
             fragment.setFragmentResultListener(requestKey) { _, bundle ->
                 listener(
                     bundle.getInt(KEY_WHICH, Activity.RESULT_CANCELED),
-                    @Suppress("DEPRECATION")
-                    bundle.getParcelable(KEY_RET)
+                    BundleCompat.getParcelable(bundle, KEY_RET, Ret::class.java)
                 )
             }
         }
@@ -45,8 +45,11 @@ abstract class AlertDialogFragment<Arg : Parcelable, Ret : Parcelable?> :
     protected abstract fun AlertDialog.Builder.prepare(listener: DialogInterface.OnClickListener)
 
     private val resultKey get() = requireArguments().getString(KEY_RESULT)
-    @Suppress("DEPRECATION")
-    protected val arg by lazy { requireArguments().getParcelable<Arg>(KEY_ARG)!! }
+    protected val arg by lazy {
+        val arguments = requireArguments()
+        BundleCompat.getParcelable(arguments, KEY_ARG, argClass)!!
+    }
+
     protected open fun ret(which: Int): Ret? = null
 
     private fun args() = arguments ?: Bundle().also { arguments = it }
