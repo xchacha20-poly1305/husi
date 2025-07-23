ae9eeca9f83df02451f723ad2db85e9611103b52
安容 <HystericalDragons@proton.me>
2025-07-23 21:35:43 +0800

    feat: wht if Material 3

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Context.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Context.kt
@@ -1,9 +1,20 @@
 package io.nekohasekai.sagernet.ktx
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
 fun Context.hasPermission(permission: String): Boolean {
     return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+}
+
+inline fun <reified T: Activity> Context.findActivity(): T? {
+    var ctx = this
+    while (ctx is ContextWrapper) {
+        if (ctx is T) return ctx
+        ctx = ctx.baseContext
+    }
+    return null
 }
