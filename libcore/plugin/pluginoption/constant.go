eee061b3db4c3854cec90518ff721ffd5c93be35
安容 <HystericalDragons@proton.me>
2025-09-06 19:52:41 +0800

    fix: registry plugin names for displaying

--- libcore/plugin/pluginoption/constant.go
+++ libcore/plugin/pluginoption/constant.go
@@ -1,6 +1,19 @@
 // Package pluginoption provides options for hooked protocols.
 package pluginoption
 
+import (
+	C "github.com/sagernet/sing-box/constant"
+)
+
 const (
 	TypeJuicity = "juicity"
 )
+
+func ProxyDisplayName(proxyType string) string {
+	switch proxyType {
+	case TypeJuicity:
+		return "Juicity"
+	default:
+		return C.ProxyDisplayName(proxyType)
+	}
+}
