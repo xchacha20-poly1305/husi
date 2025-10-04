f36459795564c0f9e5c003eef70fdb79d31f92ea
安容 <HystericalDragons@proton.me>
2025-10-04 22:42:51 +0800

    revert: downgrade compose material 3 to v1.4.0-alpha15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3:1.5.0-alpha04")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha15")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.11.0")
