654cec7d14922168c1b6de96d74ee1ceb89d50c2
安容 <HystericalDragons@proton.me>
2025-10-14 22:50:54 +0800

    fix: extra request query all packages for special vendors

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Context.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Context.kt
@@ -3,8 +3,11 @@ package io.nekohasekai.sagernet.ktx
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.provider.Settings
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 
 fun Context.hasPermission(permission: String): Boolean {
     return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
@@ -17,4 +20,16 @@ inline fun <reified T : Activity> Context.findActivity(): T? {
         ctx = ctx.baseContext
     }
     return null
+}
+
+fun Context.openPermissionSettings() {
+    if (MIUIUtils.isMIUI) runCatching {
+        MIUIUtils.openPermissionSettings(this)
+    }.onSuccess {
+        return
+    }
+
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    intent.data = "package:$packageName".toUri()
+    startActivity(intent)
 }
