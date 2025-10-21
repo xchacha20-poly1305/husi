a7bedb3a9d3182814ab7d548b877bf793c6938b1
安容 <HystericalDragons@proton.me>
2025-10-21 22:35:59 +0800

    fix: replace wrong `@StringRes`

--- app/src/main/java/io/nekohasekai/sagernet/ui/SagerViewModel.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/SagerViewModel.kt
@@ -1,7 +1,7 @@
 package io.nekohasekai.sagernet.ui
 
 import android.content.Context
-import android.support.annotation.StringRes
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.res.pluralStringResource
