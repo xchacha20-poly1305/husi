1260ae5fe828046df4729583b21f8ad639f0b206
安容 <HystericalDragons@proton.me>
2025-11-29 22:21:10 +0800

    fix(TileService): make status work

--- app/src/main/java/io/nekohasekai/sagernet/bg/TileService.kt
+++ app/src/main/java/io/nekohasekai/sagernet/bg/TileService.kt
@@ -1,16 +1,17 @@
 package io.nekohasekai.sagernet.bg
 
+import android.content.Context
 import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.repository.repo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import android.service.quicksettings.TileService as BaseTileService
 
@@ -26,13 +27,18 @@ class TileService : BaseTileService() {
     private var observeJob: Job? = null
     private val scope = CoroutineScope(Dispatchers.Main.immediate)
 
+    override fun attachBaseContext(newBase: Context) {
+        val languageContext = ContextCompat.getContextForLanguage(newBase)
+        super.attachBaseContext(languageContext)
+    }
+
     override fun onStartListening() {
         super.onStartListening()
         connection.connect(this)
         observeJob = scope.launch {
             combine(connection.status, connection.service) { status, service ->
                 status to service
-            }.collect { (status, service) ->
+            }.collectLatest { (status, service) ->
                 if (service != null) {
                     updateTile(status.state, status.profileName)
                     if (tapPending) {
@@ -59,7 +65,6 @@ class TileService : BaseTileService() {
         qsTile?.apply {
             label = null
             when (serviceState) {
-                BaseService.State.Idle -> error("serviceState")
                 BaseService.State.Connecting -> {
                     icon = iconRest
                     state = Tile.STATE_ACTIVE
