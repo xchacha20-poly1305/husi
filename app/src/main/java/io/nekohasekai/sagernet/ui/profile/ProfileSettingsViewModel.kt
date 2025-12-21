1e50efc67011ad64d66afb903f15e5fb6f00e339
安容 <HystericalDragons@proton.me>
2025-12-21 23:10:00 +0800

    chore(tuic): add more hidden congestion controls

--- app/src/main/java/io/nekohasekai/sagernet/ui/profile/ProfileSettingsViewModel.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/profile/ProfileSettingsViewModel.kt
@@ -142,13 +142,6 @@ internal val fingerprints
         SingBoxOptions.FINGERPRINT_RANDOMIZED,
     )
 
-internal val congestionControls
-    get() = listOf(
-        "bbr",
-        "cubic",
-        "new_reno",
-    )
-
 internal val muxTypes = listOf("h2mux", "smux", "yamux")
 
 internal val muxStrategies = listOf(
