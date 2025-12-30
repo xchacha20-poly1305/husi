2c855c893dcd1bfa7fb70286cd9cf22bf9b675af
安容 <HystericalDragons@proton.me>
2025-12-30 11:22:12 +0800

    fix(theme): send correct night mode status for status bar &
    recreate Activity after value changing

--- app/src/main/java/io/nekohasekai/sagernet/ui/ComposeActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ComposeActivity.kt
@@ -1,7 +1,9 @@
 package io.nekohasekai.sagernet.ui
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -11,17 +13,22 @@ import io.nekohasekai.sagernet.database.DataStore
 open class ComposeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val usingNightMode = resources.isDarkMode(DataStore.nightTheme)
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.VANILLA_ICE_CREAM) {
             // https://stackoverflow.com/questions/79319740/edge-to-edge-doesnt-work-when-activity-recreated-or-appcompatdelegate-setdefaul
             // BAKLAVA and later VANILLA_ICE_CREAM have fixed this
             // set this before super.onCreate(savedInstanceState)
             WindowCompat.setDecorFitsSystemWindows(window, false)
         }
-        enableEdgeToEdge()
+        val style = if (usingNightMode) {
+            SystemBarStyle.dark(Color.TRANSPARENT)
+        } else {
+            SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        }
+        enableEdgeToEdge(statusBarStyle = style, navigationBarStyle = style)
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val insetController = WindowCompat.getInsetsController(window, window.decorView)
-            val usingNightMode = resources.isDarkMode(DataStore.nightTheme)
             // https://dev.mi.com/xiaomihyperos/documentation/detail?pId=1576
             insetController.isAppearanceLightNavigationBars = !usingNightMode
             insetController.isAppearanceLightStatusBars = !usingNightMode
