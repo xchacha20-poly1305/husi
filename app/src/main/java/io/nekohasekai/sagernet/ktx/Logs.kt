1239bbb2d2e6e47fb3a7900db4b5bb64546792b9
安容 <HystericalDragons@proton.me>
2026-01-10 22:10:03 +0800

    chore: also write log to Android

--- app/src/main/java/io/nekohasekai/sagernet/ktx/Logs.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ktx/Logs.kt
@@ -1,5 +1,6 @@
 package io.nekohasekai.sagernet.ktx
 
+import android.util.Log
 import libcore.Libcore
 import java.io.InputStream
 import java.io.OutputStream
@@ -11,44 +12,80 @@ object Logs {
         return stackTrace[4].className.substringAfterLast(".")
     }
 
+    private fun logToAndroid(level: Int, tag: String, message: String) {
+        when (level) {
+            Log.DEBUG -> Log.d(tag, message)
+            Log.INFO -> Log.i(tag, message)
+            Log.WARN -> Log.w(tag, message)
+            Log.ERROR -> Log.e(tag, message)
+            else -> Log.println(level, tag, message)
+        }
+    }
+
     fun d(message: String) {
-        Libcore.logDebug("[${mkTag()}] $message")
+        val tag = mkTag()
+        Libcore.logDebug("[$tag] $message")
+        logToAndroid(Log.DEBUG, tag, message)
     }
 
     fun d(message: String, exception: Throwable) {
-        Libcore.logDebug("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = "$message\n${exception.stackTraceToString()}"
+        Libcore.logDebug("[$tag] $full")
+        logToAndroid(Log.DEBUG, tag, full)
     }
 
     fun i(message: String) {
-        Libcore.logInfo("[${mkTag()}] $message")
+        val tag = mkTag()
+        Libcore.logInfo("[$tag] $message")
+        logToAndroid(Log.INFO, tag, message)
     }
 
     fun i(message: String, exception: Throwable) {
-        Libcore.logInfo("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = "$message\n${exception.stackTraceToString()}"
+        Libcore.logInfo("[$tag] $full")
+        logToAndroid(Log.INFO, tag, full)
     }
 
     fun w(message: String) {
-        Libcore.logWarning("[${mkTag()}] $message")
+        val tag = mkTag()
+        Libcore.logWarning("[$tag] $message")
+        logToAndroid(Log.WARN, tag, message)
     }
 
     fun w(message: String, exception: Throwable) {
-        Libcore.logWarning("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = "$message\n${exception.stackTraceToString()}"
+        Libcore.logWarning("[$tag] $full")
+        logToAndroid(Log.WARN, tag, full)
     }
 
     fun w(exception: Throwable) {
-        Libcore.logWarning("[${mkTag()}] " + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = exception.stackTraceToString()
+        Libcore.logWarning("[$tag] $full")
+        logToAndroid(Log.WARN, tag, full)
     }
 
     fun e(message: String) {
-        Libcore.logError("[${mkTag()}] $message")
+        val tag = mkTag()
+        Libcore.logError("[$tag] $message")
+        logToAndroid(Log.ERROR, tag, message)
     }
 
     fun e(message: String, exception: Throwable) {
-        Libcore.logError("[${mkTag()}] $message" + "\n" + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = "$message\n${exception.stackTraceToString()}"
+        Libcore.logError("[$tag] $full")
+        logToAndroid(Log.ERROR, tag, full)
     }
 
     fun e(exception: Throwable) {
-        Libcore.logError("[${mkTag()}] " + exception.stackTraceToString())
+        val tag = mkTag()
+        val full = exception.stackTraceToString()
+        Libcore.logError("[$tag] $full")
+        logToAndroid(Log.ERROR, tag, full)
     }
 
 }
