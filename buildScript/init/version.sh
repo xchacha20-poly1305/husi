9dbad947737ddc398a46dd275e969b8334316018
安容 <HystericalDragons@proton.me>
2026-01-25 09:10:13 +0800

    chore: bump version to 0.16.0-alpha.0 (426)

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -7,4 +7,4 @@ GEOIP_VERSION="20260112"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20260116174239"
+GEOSITE_VERSION="20260124151154"
