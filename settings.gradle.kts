f77e704ea7cc4f29dcd9edc32126690a831196d0
安容 <HystericalDragons@proton.me>
2026-02-28 13:50:39 +0800

    chore: remove unused of compose-code-editor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,6 @@ pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.plugin.compose") version "2.3.20-RC"
         id("org.jetbrains.compose") version "1.10.1"
-        id("org.jetbrains.dokka") version "1.9.20"
     }
 }
 
