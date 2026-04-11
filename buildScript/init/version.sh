c3d26c1238a4b111de4e0976c2e0431ba7e654be
安容 <HystericalDragons@proton.me>
2026-04-11 14:46:40 +0800

    chore: bump version to 1.1.2 (453)

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.26.1"
+GO_VERSION="1.26.2"
 
 # https://github.com/Dreamacro/maxmind-geoip
 GEOIP_VERSION="20260312"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20260404050103"
+GEOSITE_VERSION="20260409151144"
