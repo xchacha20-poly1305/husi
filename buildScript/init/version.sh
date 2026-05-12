d8ad408752f4501746e8aea63439a406b9098756
安容 <HystericalDragons@proton.me>
2026-05-12 19:06:52 +0800

    chore: update dependencies
    - Update geoip & geosite
    - Update gradle dependencies

--- buildScript/init/version.sh
+++ buildScript/init/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.26.2"
+GO_VERSION="1.26.3"
 
 # https://github.com/Dreamacro/maxmind-geoip
-GEOIP_VERSION="20260412"
+GEOIP_VERSION="20260512"
 
 # https://github.com/v2fly/domain-list-community
 # or https://github.com/SagerNet/sing-geosite
-GEOSITE_VERSION="20260429071553"
+GEOSITE_VERSION="20260512024130"
