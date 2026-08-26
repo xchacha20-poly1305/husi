7086b2533e3b1dd07af01350afa656f6ee155a08
安容 <HystericalDragons@proton.me>
2026-08-26 08:23:38 +0800

    revert: "fix(hysteria): do not parse/add URL fragment"
    https://github.com/HyNetworks/hysteria-website/commit/075523560869025f1eb5020a646af9c97654c667
    
    Close https://github.com/xchacha20-poly1305/husi/issues/900
    
    This reverts commit 859df1d4334c650b54feba876933ae6cf59d37eb.

--- composeApp/src/commonMain/kotlin/fr/husi/fmt/hysteria/HysteriaFmt.kt
+++ composeApp/src/commonMain/kotlin/fr/husi/fmt/hysteria/HysteriaFmt.kt
@@ -72,7 +72,7 @@ fun parseHysteria2(link: String): HysteriaBean {
             url.username + ":" + url.password
         }
 
-        // name = url.fragment
+        name = url.fragment
 
         sni = url.queryParameter("sni")
         allowInsecure = url.parseBoolean("insecure")
@@ -108,13 +108,13 @@ fun HysteriaBean.toUri(): String {
         username = authPayload
     }
 
+    if (name.isNotBlank()) {
+        url.fragment = name
+    }
     if (allowInsecure) {
         url.addQueryParameter("insecure", "1")
     }
     if (protocolVersion == HysteriaBean.PROTOCOL_VERSION_1) {
-        name.blankAsNull()?.let {
-            url.fragment = it
-        }
         if (sni.isNotBlank()) {
             url.addQueryParameter("peer", sni)
         }
